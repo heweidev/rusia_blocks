55276c9f3effd91ddd6fee42e71d92a11f3d6343
hewei <netfield@163.com>
2015-01-22 17:06:03 +0800

    project config

--- RusiaBlocks/src/com/hewei/rusiablocks/WallView.java
+++ RusiaBlocks/src/com/hewei/rusiablocks/WallView.java
@@ -4,7 +4,6 @@ import com.hewei.rusiablocks.block.Block;
 import com.hewei.rusiablocks.block.Wall;
 
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,7 +12,6 @@ import android.graphics.Paint.Style;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 public class WallView extends View {
